a7cce6577d78a2591d0b4520f10e6a6305865d85
zhongxueyu <2329773501@qq.com>
2017-04-18 22:26:44 +0800

    use simple_format helper to decorate

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,6 +1,29 @@
 class Account::PostsController < ApplicationController
+  before_action :find_post_and_check_permission, only: [:edit, :destroy]
+
   before_action :authenticate_user!
   def index
     @posts = current_user.posts
   end
+
+  def edit
+
+  end
+
+private
+
+def find_post_and_check_permission
+  @group = Group.find(params[:group_id])
+  @post = Post.new(post_params)
+
+    if current_user != @post.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
+
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
